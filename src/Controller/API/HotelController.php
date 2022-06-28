f587b4490ea5ba2401c57b5cc36b022c6c367b72
khoatrannfq <dangtran.khoa@nfq.asia>
2022-06-28 13:10:45 +0700

    feat:list cars and details car

--- src/Controller/API/HotelController.php
+++ src/Controller/API/HotelController.php
@@ -2,15 +2,62 @@
 
 namespace App\Controller\API;
 
+use App\Entity\Hotel;
+use App\Request\Hotel\ListHotelRequest;
+use App\Service\HotelService;
+use App\Traits\JsonResponseTrait;
+use App\Transformer\DetailHotelTransformer;
+use App\Transformer\ListHotelTransformer;
+use App\Transformer\ValidatorTransformer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/hotels', name: 'hotel_')]
 class HotelController extends AbstractController
 {
-    #[Route('/', name: 'list')]
-    public function index(): Response
+    use JsonResponseTrait;
+
+    private ValidatorTransformer $validatorTransformer;
+    private ValidatorInterface $validator;
+
+    public function __construct(ValidatorTransformer $validatorTransformer, ValidatorInterface $validator)
     {
+        $this->validatorTransformer = $validatorTransformer;
+        $this->validator = $validator;
+    }
+
+    #[Route('', name: 'list', methods: ['GET'])]
+    public function index(
+        Request $request,
+        ListHotelRequest $listHotelRequest,
+        HotelService $hotelService,
+        ListHotelTransformer $listHotelTransformer
+    ): Response {
+        $filters = $request->query->all();
+        $hotelRequest = $listHotelRequest->fromArray($filters);
+        $errors = $this->validator->validate($hotelRequest);
+        if (count($errors) > 0) {
+            $errorsTransformer = $this->validatorTransformer->toArray($errors);
+
+            return $this->error($errorsTransformer);
+        }
+        $hotels = $hotelService->findAll($hotelRequest);
+        $hotelsResult = $listHotelTransformer->listToArray($hotels);
+
+        return $this->success($hotelsResult);
+    }
+
+    #[Route('/{id}', name: 'detail', methods: ['GET'])]
+    public function detail(
+        Hotel $hotel,
+        ListHotelTransformer $hotelTransformer,
+        HotelService $hotelService
+    ): JsonResponse {
+        $hotel = $hotelService->detail($hotel);
+        return $this->success($hotelTransformer->toArray($hotel));
     }
 }
