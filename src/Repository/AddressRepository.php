763b7310c7c4d73b105bee283b61c0c0fd53428b
Khoa Tran <105048891+khoatrannfq@users.noreply.github.com>
2022-06-30 13:09:39 +0700

    feat: list city and search city

--- src/Repository/AddressRepository.php
+++ src/Repository/AddressRepository.php
@@ -3,7 +3,11 @@
 namespace App\Repository;
 
 use App\Entity\Address;
+use App\Entity\Hotel;
+use App\Request\City\ListCityRequest;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -16,8 +20,38 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class AddressRepository extends BaseRepository
 {
+    public const ADDRESS_ALIAS = 'ad';
+
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Address::class);
+        parent::__construct($registry, Address::class, static::ADDRESS_ALIAS);
+    }
+
+    public function list(ListCityRequest $cityRequest)
+    {
+        $addressQuery = $this->createQueryBuilder(static::ADDRESS_ALIAS)
+            ->select('ad.city, count(h.id) as count_hotel')
+            ->join(Hotel::class, 'h', Join::WITH, 'ad.hotel=h.id')
+            ->groupBy('ad.city')
+            ->orderBy('count_hotel', 'desc');
+        $searchCity = $cityRequest->getSearch();
+        if (null == $searchCity) {
+            return $this->listTopCity($addressQuery, $cityRequest);
+        }
+        return $this->searchCity($addressQuery, $searchCity);
+    }
+
+    private function listTopCity(QueryBuilder $addressQuery, ListCityRequest $cityRequest): array
+    {
+        return $addressQuery->select('ad.city, count(h.id) as count_hotel')
+            ->setMaxResults($cityRequest->getLimit())->setFirstResult($cityRequest->getOffset())
+            ->getQuery()->getResult();
+    }
+
+    private function searchCity(QueryBuilder $addressQuery, $searchCity)
+    {
+        return $addressQuery->where('ad.city LIKE :city')->setParameter('city', '%' . $searchCity . '%')
+            ->getQuery()->getResult();
+
     }
 }
