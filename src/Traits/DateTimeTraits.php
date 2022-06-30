ae496026542ef64b2408480c004c8ab32e0d850a
khoatrannfq <dangtran.khoa@nfq.asia>
2022-06-30 21:38:13 +0700

    fix: convert timestamp to datetime

--- src/Traits/DateTimeTraits.php
+++ src/Traits/DateTimeTraits.php
@@ -6,6 +6,6 @@ trait DateTimeTraits
 {
     public function timestampToDateTime(int $timestamp): string
     {
-        return date('Y/m/d', 1541843467);
+        return date('Y/m/d', $timestamp);
     }
 }
