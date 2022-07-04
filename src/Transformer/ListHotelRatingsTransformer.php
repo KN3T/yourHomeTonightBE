dac32f692c70b9b69df33e5d2c0edd545dac8c2c
tringuyennfq <khai.tri@nfq.asia>
2022-07-04 11:54:04 +0700

    change response

--- src/Transformer/ListHotelRatingsTransformer.php
+++ src/Transformer/ListHotelRatingsTransformer.php
@@ -2,15 +2,26 @@
 
 namespace App\Transformer;
 
+use App\Entity\Rating;
+
 class ListHotelRatingsTransformer extends BaseTransformer
 {
+    private UserTransformer $userTransformer;
+
+    public function __construct(UserTransformer $userTransformer)
+    {
+        $this->userTransformer = $userTransformer;
+    }
+
     public const ALLOW = ['id', 'content', 'rating', 'createdAt', 'updatedAt'];
 
     public function toArray(array $hotelRating): array
     {
         $ratingEntity = $hotelRating['review'];
+        $user = $ratingEntity->getBooking()->getUser();
         $ratingArray = $this->transform($ratingEntity, ListHotelRatingsTransformer::ALLOW);
         $ratingArray['roomNumber'] = $hotelRating['roomNumber'];
+        $ratingArray['user'] = $this->userTransformer->toArray($user);
         return $ratingArray;
     }
 
